b124c907b836fd180700e947e2ef144623a8db2f
arunkhattri <arunkhattri@outlook.com>
2024-03-06 21:33:02 +0530

    xml file key is validated

--- parse_xml.py
+++ parse_xml.py
@@ -8,18 +8,11 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from datetime import datetime
 import xmlsec
-from signxml import XMLVerifier, SignatureConfiguration, SignatureMethod
 
 # offline e-KYC
-aadhaar_xml = "./data/offlineaadhaar20240304020624525.xml"
 ak_xml = "./data/arun_eKYC.xml"
 # suggested public key on e-KYC page of UIDAI
-cer_file = "./data/uidai_auth_sign_prod_2023.cer"
-# downloaded from download section of UIDAI
-cer2026 = "./data/uidai_offline_publickey_17022026.cer"
-# public key, downloaded from UIDAI
 cer_feb_24 = "./data/uidai_offline_publickey_26022021.cer"
-another_cer = "./data/uidai_auth_sign_prod.cer"
 
 # ---------------------------------------
 # Parse xml for required data
@@ -74,7 +67,8 @@ def validate_xml(xml_file):
     if uiddata is None:
         raise ValueError("UidData element not found in the XML file")
     # serialize the UidData element( excluding Signature )
-    data = etree.tostring(uiddata, method="c14n", exclusive=True, with_comments=False)
+    # data = etree.tostring(uiddata, method="c14n2", exclusive=True, with_comments=False)
+    data = etree.canonicalize(uiddata)
 
     # Signature Element
     sign_node = xmlsec.tree.find_node(root, xmlsec.constants.NodeSignature)
@@ -84,7 +78,7 @@ def validate_xml(xml_file):
     # DigestValue
     dv = xmlsec.tree.find_node(sign_node, "DigestValue").text
     db_base64 = base64.b64decode(dv)
-    hashed_data = sha256(data).hexdigest()
+    hashed_data = sha256(data.encode()).hexdigest()
 
     if db_base64 == hashed_data:
         print("XML signature is valid")
@@ -146,28 +140,6 @@ def verify_mobile_email(mobile, email, pass_code, last_digit_of_uid):
 
 
 # ---------------------------------------
-# validate signature
-def validate_certificate(cert_path):
-    with open(cert_path, "rb") as cert_file:
-        cert_data = cert_file.read()
-
-    cert = x509.load_pem_x509_certificate(cert_data, default_backend())
-
-    # Validate the certificate
-    # You can perform various checks here based on your requirements
-    # For example, you can check if the certificate is expired, trusted,
-    # issued by a specific CA, etc.
-
-    # Check if the certificate is expired
-    if cert.not_valid_after_utc < datetime.now(cert.not_valid_after.tzinfo):
-        print("Certificate has expired!")
-        return False
-
-    # Check if the certificate is trusted (You can implement your own trust store logic)
-    # For demonstration purposes, let's assume all certificates are trusted
-    print("Certificate is trusted!")
-
-    return True
 
 
 def certificate_to_base64(cert_path):
@@ -180,56 +152,31 @@ def certificate_to_base64(cert_path):
 
 
 # Example usage
-base64_cert_string = certificate_to_base64(cer_file)
+base64_cert_string = certificate_to_base64(cer_feb_24)
 print(base64_cert_string)
 
-
-# Example usage
-
-if validate_certificate(cer2026):
-    print("Certificate is valid!")
-else:
-    print("Certificate validation failed!")
-
-with open(cer2026, "rb") as cert_file:
-    cert_data = cert_file.read()
-
-cert = x509.load_pem_x509_certificate(cert_data, default_backend())
-# cert = x509.load_der_x509_certificate(cer2026, default_backend())
-
-public_key = cert.public_key()
-isinstance(public_key, rsa.RSAPublicKey)
 
 # =================================
 # xmlsec
 # =================================
 
+
 # sign_node = xmlsec.tree.find_node(root, xmlsec.constants.NodeSignatureValue)
-uidai_cert = "./data/uidai_auth_sign_prod_2023.cer"
+def verify_key(xml_file, key):
+    tree = etree.parse(xml_file)
+    root = tree.getroot()
+    sign_node = xmlsec.tree.find_node(root, xmlsec.constants.NodeSignature)
+    ctx = xmlsec.SignatureContext()
+    key = xmlsec.Key.from_file(key, xmlsec.constants.KeyDataFormatCertPem)
+    ctx.key = key
 
-sign_node = xmlsec.tree.find_node(root, xmlsec.constants.NodeSignature)
-ctx = xmlsec.SignatureContext()
-key = xmlsec.Key.from_file(cer_feb_24, xmlsec.constants.KeyDataFormatCertPem)
-ctx.key = key
+    try:
+        ctx.verify(sign_node)
+        print("verified")
+        return 0
+    except Exception as e:
+        print(f"[e] {e}")
+        return 1
 
-try:
-    ctx.verify(sign_node)
-except Exception as e:
-    print(f"[e] {e}")
 
-# =================================
-# xmlverify
-# =================================
-# cer_feb_24 not working
-# cer_file also not working
-# Signature method RSA_SHA1 forbidden by configuration
-with open(cer_file, "rb") as f:
-    key = f.read()
-
-# ec = SignatureConfiguration(signature_methods=SignatureMethod.RSA_SHA1)
-
-try:
-    verify = XMLVerifier().verify(root, x509_cert=key)
-    print(f"[-] Data is {verify}")
-except Exception as e:
-    print(f"[E] Error: {str(e)}")
+verify_key(ak_xml, cer_feb_24)
